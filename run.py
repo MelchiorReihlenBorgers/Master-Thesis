a61149e68754a12f70473512872704700a9a54f3
Melchior Reihlen-Börgers <m.reihlenboergers@gmail.com>
2020-07-24 15:27:03 +0200

    initial commit

--- run.py
+++ run.py
@@ -5,13 +5,13 @@ Main file of the project.
 import os
 from DataLoad import DataLoad
 
-path = os.getcwd()
 
+# Load the data
+path = os.getcwd()
 data = DataLoad(path=os.getcwd())
 
 n_total = len(data.extract_paths()[0])  # If you want to load all images.
 
 images, depths, radians = data.load_data(N=3)
 
-paths = data.extract_paths()
-print(paths[ 0 ][ :2 ])
+
