bf1dfeedfc1a466b4a476bfd10432507c3baca5a
Melchior Reihlen-Börgers <m.reihlenboergers@gmail.com>
2020-07-28 15:09:44 +0200

    Added function to check intersection of two rectangles.

--- BayesianEstimation.py
+++ BayesianEstimation.py
@@ -1,27 +1,33 @@
+import os
 import numpy as np
+import pandas as pd
 
-def simulate_windows(N = 100):
+def simulate_windows(N = 1):
     """
-    :param N: Number of windwos to estimate. Default = 100
+    :param N: Number of windwos to estimate. Default = 1
     :return: Three lists, with the top left coordinate, the width and the height of an estimated window
     """
     # TODO: The returns should depend on the input image. Thus, this should be a class, where the input
     # TODO: is the image from the DataLoad class.
     # TODO: Add Xdim, ydim as attributes in DataLoad
-    widths = [np.random.randint(low = 500, high = 3000, size = 1) for _ in range(N)]
+    width = np.random.randint(low = 500, high = 3000, size = 1)
 
-    heights = [np.random.randint(low = 300, high = 2000, size = 1) for _ in range(N)]
+    height = np.random.randint(low = 300, high = 2000, size = 1)
 
-    x_coordinate = [np.random.randint(high = 4032 - widths[i], low = 0, size = 1) for i in range(N)]
+    x_coordinate = np.random.randint(high = 4032 - width, low = 0, size = 1)
 
-    y_coordinate = [np.random.randint(high = 3024 - heights[i], low = 0, size = 1) for i in range(N)]
+    y_coordinate = np.random.randint(high = 3024 - height, low = 0, size = 1)
 
-    return widths, heights, x_coordinate, y_coordinate
+    return width, height, x_coordinate, y_coordinate
 
 
-widths, heights, x, y = simulate_windows(N = 10)
 
-def does_intersect(annotation):
+if os.path.exists(os.path.join(os.getcwd(), "test_annotation.csv")):
+    annotation = pd.read_csv("test_annotation.csv",
+                             header = None,
+                             names = ["Label", "X", "Y", "width", "height", "Image_Name", "Xdim", "Ydim"])
+
+def does_intersect(annotation_x, annotation_y, annotation_width, annotation_height):
     """
     Check whether two boxes overlap or not.
     The .csv exported from makesense.ai has the following columns:
@@ -38,7 +44,45 @@ def does_intersect(annotation):
     :return: List of boolean values indicating whether a window intersects with the annotated window.
     """
 
-    pass
+    width, height, x, y = simulate_windows(N = 1)
+
+    # Get the top left and bottom right corner of both the annotated window and the simulated window.
+    tl_sim = (x, y)
+    br_sim = (x + width, y + height)
+
+    tl_annotation = (annotation_x, annotation_y)
+    br_annotation = (annotation_x + annotation_width, annotation_y + annotation_height)
+
+    # Code the conditions
+    sim_right_of_annotation = tl_sim[0] >= br_annotation[0]
+    sim_left_of_annotation = tl_annotation[0] >= br_sim[0]
+
+    ## Be careful here. Images start at zero, zero in the top left corner -->  going to the bottom left corner means going up the y values.
+    sim_above_annotation = br_sim[1] <= tl_annotation[1]
+    sim_below_annotation = br_annotation[1] <= tl_sim[1]
+
+    # If one rectangle is on left side of other
+    if (sim_right_of_annotation or sim_left_of_annotation):
+        intersection = False
+
+    # If one rectangle is above other
+    if (sim_above_annotation or sim_below_annotation):
+        intersection = False
+
+    """
+    # In case you want to print everything
+    print("Width Simulated: {} \n Height Simulated: {} \n X Simulated: {} \n Y Simulated: {}".format(width, height, x, y))
+    print("------------------------------------------------------------------------ \n------------------------------------------------------------------------")
+    print(" Width Annotated: {} \n Height Annotated: {} \n X Annotated: {} \n Y Annotated: {}".format(annotation_width, annotation_height, annotation_x, annotation_y))
+    print("------------------------------------------------------------------------ \n------------------------------------------------------------------------")
+    print("Top Left Simulated: {} \n Bottom Right Simulated: {} \n Top Left Annotated: {} \n Bottom Right Annoted: {}".format(tl_sim, br_sim, tl_annotation, br_annotation))
+    """
+    intersection = True
+
+    return intersection, width, height, x, y
+
+
+a = does_intersect(annotation_x= 1, annotation_y=10, annotation_height=10, annotation_width = 10)
 
 def compute_area():
     """
@@ -74,3 +118,7 @@ def compute_features():
     """
 
     pass
+
+
+if __name__ == "__main__":
+    widths, heights, x, y = simulate_windows(N=10)
