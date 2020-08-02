a62fea1ee1c895a38d98f23889ce691b038d0769
Melchior Reihlen-Börgers <m.reihlenboergers@gmail.com>
2020-08-02 17:12:04 +0200

    intial commit for feature extraction and saliency map estimation script

--- training/extract_features.py
+++ training/extract_features.py
@@ -1,9 +1,10 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 
 from training.saliency_map_estimation import saliency_map_estimation
 
-def extract_features(image_name, examples_dict):
+def extract_features(depth, image, dict_entry):
     """
     Extract features from the different windows selected.
 
@@ -13,15 +14,51 @@ def extract_features(image_name, examples_dict):
     The intention is that this is to be used in an iterative process where the function is called upon iteratively.
 
     :param image: Image to be used
-    :param examples_dict: Dictionary with the positive and negative examples created in training_run.py
+    :param dict_entry: Dictionary entry containing (in that order) width, height, x and y coordinate.
     """
-    path_image = os.path.join(os.getcwd(), image_name)
-    image = plt.imread(path_image)
 
-    saliency_map_estimation(image = image)
+    # Make sure that all of the values are integers as otherwhise they are saved as arrays
+    # which can not be used to slice arrays.
+    width, height, x, y = int(dict_entry[0]), int(dict_entry[1]), int(dict_entry[2]), int(dict_entry[3])
 
-    # TODO: Connect this to the DataLoad() module. You should not have two different data loads. Maybe sth along the lines of find the path that ends on image_name and use that
-    # TODO: picture and depth.
+    # Get percentage of coordinates for depth map, i.e. x percent of the length of the image and y percent of the height to
+    # map this on the smaller depth map.
+
+    xdim_percentage = x/image.shape[1]
+    ydim_percentage = y/image.shape[0]
+    width_percentage = width / image.shape[0]
+    height_percentage = height/image.shape[1]
 
 
+    x_depth = int(xdim_percentage * depth.shape[1])
+    y_depth = int(ydim_percentage * depth.shape[0])
+    width_depth = int(width_percentage * depth.shape[1])
+    height_depth = int(height_percentage * depth.shape[0])
+
+    # Get the relevant part of the depth map.
+    depth = np.array(depth)
+    depth = depth[y_depth:y_depth+height_depth, x_depth:x_depth+width_depth]
+
+    # Resize the image to only compute the saliency on the parts that are relevant
+    image = image[y:y + height, x:x + width, :]
+
+    saliencyMap = saliency_map_estimation(image = image)
+
+
+    # Compute the mean depth and saliency:
+    mean_depth = np.mean(depth)
+    mean_saliency = np.mean(saliencyMap)
+
+    return mean_depth, mean_saliency
+
+
+
+if __name__ == "__main__":
+    from DataLoad import DataLoad
+
+    data = DataLoad(path="/media/melchior/Elements/MaastrichtUniversity/BISS/MasterThesis")
+
+    images, depth, radian = data.load_data(N = 1)
+
+    mean_depth, mean_saliency = extract_features(depth[0], images[0], examples['585216074-0.jpg'][0][0])
 
--- training/training_run.py
+++ training/training_run.py
@@ -3,18 +3,26 @@ import pandas as pd
 import numpy as np
 
 from training.plotting import  plotting_windows
-
 from training.create_training_sample import create_training_sample
+from training.extract_features import extract_features
 
+from DataLoad import DataLoad
+
+
+# Load the .csv file with annotations (obtained from makesense.ai)
 if os.path.exists("/media/melchior/Elements/MaastrichtUniversity/BISS/MasterThesis/test_annotation.csv"):
-    annotations = pd.read_csv("test_annotation.csv",
+    annotations = pd.read_csv("/media/melchior/Elements/MaastrichtUniversity/BISS/MasterThesis/test_annotation.csv",
                               header = None,
                               names = ["Label", "X", "Y", "width", "height", "Image_Name", "Xdim", "Ydim"])
 
+# Compute the mean annotation width and height and subtract the standard deviation once to
+# use it as a lower bound in the sampling of random windows in the image.
 mean_annotation_width = np.mean(annotations.loc[:,"width"]) - np.std(annotations.loc[:,"width"])
 mean_annotation_height = np.mean(annotations.loc[:,"height"]) - np.std(annotations.loc[:,"height"])
 
-
+# Save the number of rows in the annotations csv file.
+# This is used in the subsequent loop to sample 100 random windows per annotation (one annotation per image so far...)
+# TODO: Increase K to 10K to make a more reasonable training sample. For now its fine for speed.
 nrow = annotations.shape[0]
 
 examples = {}
@@ -24,7 +32,7 @@ for row in range(nrow):
                                                                       annotations.loc[row,"height"]
 
     positive_examples, negative_examples = create_training_sample(annotation_x, annotation_y, annotation_width, annotation_height,
-                                                                  K = 10000,
+                                                                  K = 100,
                                                                   theta = 0.4,
                                                                   width_low = mean_annotation_width,
                                                                   height_low = mean_annotation_height)
@@ -35,10 +43,43 @@ for row in range(nrow):
 
 
 # Plotting
+# Plot one example just to show how it works.
 x, y, width, height = positive_examples[0][2], positive_examples[0][3], positive_examples[0][0], positive_examples[0][1]
 annotation_x, annotation_y, annotation_width, annotation_height = annotations.loc[row,"X"], annotations.loc[row,"Y"], annotations.loc[row,"width"], annotations.loc[row,"height"]
 
 plotting_windows("/media/melchior/Elements/MaastrichtUniversity/BISS/MasterThesis/test_image.jpg",
                  x, y, width, height,
                  annotation_x, annotation_y, annotation_width, annotation_height)
+
+
+# Load the first 3 images to create a training set.
+# TODO: This has to be done with at least 50 images. To obtain that you need to annotate the first 50 images as well.
+data = DataLoad(path="/media/melchior/Elements/MaastrichtUniversity/BISS/MasterThesis")
+images, depth, radian = data.load_data(N = 3)
+
+
+# Extract the features from the samples and save them to create the input data for the algorithm.
+# mean_saliency, mean_depth = extract_features()
+
+image_names = list(examples.keys())
+all_features = []
+
+# Iterate over different images
+for index, name in enumerate(image_names):
+
+    # Each image has positive and negative examples/windows that cover more than theta percent of the annotation and not
+    for example in range(2):
+        windows_example_list = len(examples[name][example])
+
+        # Extract the features from each of these windows and save them as a list.
+        for window in range(windows_example_list):
+            mean_depth, mean_saliency = extract_features(depth[index], images[index], examples[name][example][window])
+
+            features = (mean_depth, mean_saliency)
+
+            all_features.append(features)
+
+# TODO: Bind all of these into one data frame.
+
+# TODO: Get target from dict.
 
