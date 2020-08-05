f399b7f9231084f5ca2ca234e1eded723dc256fe
Melchior Reihlen-Börgers <m.reihlenboergers@gmail.com>
2020-08-05 10:43:03 +0200

    adjusted for changes in how classification is done
    saved data as csv to reduce overhead

--- training/training_run.py
+++ training/training_run.py
@@ -2,13 +2,13 @@ import os
 import numpy as np
 import pandas as pd
 
-from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 
 from training.create_training_sample import create_training_sample
 from training.extract_features import get_features
-from training.make_classifications import make_classifications
+
+from training.Classification import Classification
 
 from DataLoad import DataLoad
 
@@ -32,6 +32,7 @@ label_dictionary, labels, positive_examples, negative_examples = create_training
 data = DataLoad(path="/media/melchior/Elements/MaastrichtUniversity/BISS/MasterThesis")
 images, depth, radian = data.load_data(N = 50)
 
+print("{} Images, {} Depths and {} Radians measures were loaded".format(len(images), len(depth), len(radian)))
 
 # Extract the features from the samples and save them to create the input data for the algorithm.
 # mean_saliency, mean_depth = extract_features()
@@ -46,7 +47,8 @@ mean_depth = [all_features[i][0] for i in range(N)]
 mean_saliency = [all_features[i][1] for i in range(N)]
 color_distance = [all_features[i][2] for i in range(N)]
 edge_density = [all_features[i][3] for i in range(N)]
-superpixel_straddeling = [all_features[i][4] for i in range(N)]
+
+#superpixel_straddeling = [all_features[i][4] for i in range(N)]
 
 
 data = pd.DataFrame()
@@ -54,26 +56,35 @@ data["Mean_Saliency"] = mean_saliency
 data["Mean_Depth"] = mean_depth
 data["Color_Distance"] = color_distance
 data["Edge_Density"] = edge_density
-data["Superpixel_Straddling"] = superpixel_straddeling
+# data["Superpixel_Straddling"] = superpixel_straddeling
 data["label"] = labels
 
+
+################### ----------------------- ###################
+###################    !START FROM HERE!    ###################
+################### ----------------------- ###################
+
+data.to_csv("/media/melchior/Elements/MaastrichtUniversity/BISS/MasterThesis/data.csv")
+
+data = pd.read_csv("/media/melchior/Elements/MaastrichtUniversity/BISS/MasterThesis/data.csv",
+                   header = 0)
+
 # Implement classification.
-X = data.loc[:,["Mean_Saliency", "Mean_Depth", "Color_Distance", "Edge_Density", "Superpixel_Straddling"]]
+X = data.loc[:,["Mean_Saliency", "Mean_Depth", "Color_Distance", "Edge_Density"]]
 y = data.loc[:,"label"]
-X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2)
 
-
-# Classifier
 nb = GaussianNB()
 kmeans = KNeighborsClassifier()
 
-# TODO: Write this in function or in loop at the very least
-classifier, accuracy_in_sample, accuracy_out_of_sample, pred_in_sample, pred_out_of_sample = make_classifications(nb, X_train, X_test, y_train, y_test)
-classifier, accuracy_in_sample, accuracy_out_of_sample, pred_in_sample, pred_out_of_sample = make_classifications(kmeans, X_train, X_test, y_train, y_test)
+methods = [ nb, kmeans ]
+
+results = [ ]
+
+for method in methods:
+    classification = Classification(method=method, X=X, y=y)
+
+    result = classification.evaluation("test")
+
+    results.append(result)
 
 
-
-import matplotlib.pyplot as plt
-
-plt.scatter(x = mean_depth, y = np.log(color_distance), c = labels)
-plt.show()
